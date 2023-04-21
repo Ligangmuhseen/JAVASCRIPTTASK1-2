e5efcfbc956da49d007dbb192e90e6c745cb8981
Ligangmuhseen <triplerainbow07@gmail.com>
2023-04-22 00:02:42 +0300

    my first commit on javascript tasks

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function myfunction() {
+    
+    document.querySelectorAll("img")[0].setAttribute("src","https://media.giphy.com/media/jaqvaWqpKflmQ/giphy.gif");
+    document.querySelectorAll("img")[1].setAttribute("src","https://media.giphy.com/media/jaqvaWqpKflmQ/giphy.gif");
+    document.querySelectorAll("img")[2].setAttribute("src","https://media.giphy.com/media/jaqvaWqpKflmQ/giphy.gif");
+    document.querySelector("p").style.textTransform = "upperCase";
+
+}
+
+myfunction();
